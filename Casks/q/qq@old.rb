d0bb6d1a4dc27ecfc8abca6827606f8a9dff4a5e
edgeworthlauzero <edgeworthlau@outlook.com>
2026-03-21 11:19:59 +0800

    Rearrange rb files for Casks

---
+++ Casks/q/qq@old.rb
@@ -0,0 +1,29 @@
+cask "qq@old" do
+  version "6.9.80_250904_01"
+  sha256 "5548e059023169b8d7917261c5089026560391b2c334b69541e45a678586388b"
+
+  url "https://dldir1v6.qq.com/qqfile/qq/QQNT/Mac/QQ_#{version}.dmg"
+  name "QQ"
+
+  auto_updates true
+
+  app "QQ.app"
+
+  uninstall quit: "com.tencent.qq"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.tencent.qq",
+    "~/Library/Application Scripts/FN2V63AD2J.com.tencent.localserver2",
+    "~/Library/Application Scripts/FN2V63AD2J.com.tencent.ScreenCapture2",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tencent.qq.sfl*",
+    "~/Library/Caches/com.tencent.qq",
+    "~/Library/Containers/com.tencent.qq",
+    "~/Library/Containers/com.tencent.qq.share",
+    "~/Library/Containers/FN2V63AD2J.com.tencent.localserver2",
+    "~/Library/Containers/FN2V63AD2J.com.tencent.ScreenCapture2",
+    "~/Library/Group Containers/FN2V63AD2J.com.tencent",
+    "~/Library/Preferences/com.tencent.qq.plist",
+    "~/Library/Saved Application State/com.tencent.qq.savedState",
+    "~/Library/WebKit/com.tencent.qq",
+  ]
+end
